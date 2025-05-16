9fef439cbe2b153c368b4bd88125dc0a455c23f2
LUO KUN <raphael.kun.luo@outlook.com>
2025-05-16 22:40:32 +0800

    feat:add test code
    练习HTML、CSS、JS的代码也是非常珍贵，不能不留

---
+++ overalltest/test.js
@@ -0,0 +1,14 @@
+let i=2;
+const arr=[9,2,6,4,5,8,6];
+const author="Nexus Raphael";
+let num=586;
+let x=null;
+let y=undefined;
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof arr)
+console.log(typeof author)
+
+console.log("My name is ${author}")
+arr.push(5,2,0)
+console.log(author.split(" "));

a6659b20c5793675d894f7da8a0cc175a084a94a
LUO KUN <raphael.kun.luo@outlook.com>
2025-05-17 17:24:44 +0800

    chore

--- src/prjs.c
+++ src/prjs.c
@@ -5,9 +5,12 @@
 int main(){
     cJSON* root =cJSON_CreateObject();
     cJSON_AddStringToObject(root,"str1","你好，巴巴尔！");
-    cJSON_AddNumberToObject(root,"num",586);
+    cJSON* key=cJSON_AddNumberToObject(root,"num",586);
     char *str=cJSON_Print(root);
-
+    if(key!=NULL){
+        printf("key:%s\nvalue:%d\n",key->string,key->valueint);
+    }
+    printf("______________________\n");
     printf(str);
     free(str);
 

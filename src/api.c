3eac106a64f12699496de5f28b2f70b3fa80afee
LUO KUN <raphael.kun.luo@outlook.com>
2025-05-19 20:40:07 +0800

    feat: add function 2.

--- src/api.c
+++ src/api.c
@@ -53,3 +53,32 @@ char *fetch_user_info(const char *handle) {
     curl_easy_cleanup(curl);
     return s.ptr;
 }
+
+char *fetch_contest_list(void) {
+    CURL *curl = curl_easy_init();
+    if (!curl) return NULL;
+
+    struct string s = { .ptr = malloc(1), .len = 0 };
+    char errbuf[CURL_ERROR_SIZE] = {0};
+    const char *url = "https://codeforces.com/api/contest.list";
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
+    curl_easy_setopt(curl, CURLOPT_CAINFO, "certs/cacert.pem");
+
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl error: %s\n", errbuf[0] ? errbuf : curl_easy_strerror(res));
+        if (s.len > 0) {
+            fprintf(stderr, "  >>> raw response:\n%s\n", s.ptr);
+        }
+        free(s.ptr);
+        curl_easy_cleanup(curl);
+        return NULL;
+    }
+
+    curl_easy_cleanup(curl);
+    return s.ptr;
+}
